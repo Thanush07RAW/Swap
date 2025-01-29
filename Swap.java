18679010c1fa6f7d546dfdb7c8f949c3037c5551
Thanush07RAW <draken0067@gmail.com>
2025-01-29 21:53:21 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,9 @@
+public class Swap {
+    public static void main(String[] args){
+        int x = 8,y = 15;
+        x = x+y;
+        y = x-y;
+        x = x-y;
+        System.out.println("x ="+ x +",y="+y); 
+    }
+}
